8ef4a0bc3f4c5384f03027caedbf6eecdd6acc2d
JFJ0831 <cq95678@googlemail.com>
2018-01-15 11:16:48 +0100

    version 1.0

--- A50_Römische_Zahldarstellung/src/ZahlumwandlungTest.java
+++ A50_Römische_Zahldarstellung/src/ZahlumwandlungTest.java
@@ -1,6 +1,7 @@
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
@@ -14,10 +15,27 @@ public class ZahlumwandlungTest {
 		assertThat("Zahl 1 wurde nicht korrekt umgewandelt.",
 				eineZahlumwandlung.wandeleZahlUm(1),
 				is(equalTo("I")));
+		
+		assertThat("Zahl 1 wurde nicht korrekt umgewandelt.",
+				eineZahlumwandlung.wandeleZahlUm(3000),
+				is(equalTo("MMM")));
+		
+		assertThat("Zahl 1 wurde nicht korrekt umgewandelt.",
+				eineZahlumwandlung.wandeleZahlUm(2018),
+				is(equalTo("MMXVIII")));
+		
+		assertThat("Zahl 1 wurde nicht korrekt umgewandelt.",
+				eineZahlumwandlung.wandeleZahlUm(34),
+				is(equalTo("XXXIV")));
+		
+		assertThat("Zahl 1 wurde nicht korrekt umgewandelt.",
+				eineZahlumwandlung.wandeleZahlUm(99),
+				is(equalTo("XCIX")));
 
-		/*
-		 * Tests hier ergaenzen.
-		 */
+		Throwable ex = assertThrows(IllegalArgumentException.class, () -> {eineZahlumwandlung.wandeleZahlUm(3500);});
+		assertThat("I wurde nicht korrekt verarbeitet.",
+				ex.getMessage(),
+				is(equalTo("Zahl liegt nicht im Bereich 1 bis 3000.")));
 
 	}
 }
