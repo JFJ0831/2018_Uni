08299905de8e49410ec4a68119947b14b17931fb
JFJ0831 <cq95678@googlemail.com>
2018-04-29 06:27:02 +0200

    v1.0

--- A07_ADTQueue/src/Warteschlange.java
+++ A07_ADTQueue/src/Warteschlange.java
@@ -3,8 +3,78 @@
  *  Die Implementierung nutzt zwei Stapel.
  */
 public class Warteschlange<T> implements ADTQueue<T> {
+	
+	// Erzeugt jeweils einen Stapel dessen oberstes Element später das erste bzw. das letzte Element der Warteschlange ist.
+	private Stapel<T> upperfirstofqueue = new Stapel<T>();
+	private Stapel<T> upperlastofqueue = new Stapel<T>();
 
-  /*
-   *  Hier Programmtext ergaenzen.
-   */
+	public Warteschlange() {
+		
+	}
+	
+	/**
+	 * Legt die Elemente des from-Stapels in umgekehrter Reihenfolge auf den to-Stapel.
+	 * @param from Der Stapel der umgeschichtet werden soll.
+	 * @param to Der Zielstapel des Umschichtens.
+	 * @throws IllegalStateException falls der Zielstapel (to) nicht leer ist.
+	 */
+	private void restack(Stapel<T> from, Stapel<T> to) throws IllegalStateException {
+		if (!to.isEmpty() && !from.isEmpty()) {
+			throw new IllegalStateException("Umschichten fehlgeschlagen, da Zielstapel nicht leer.");
+		}
+		else {
+			while (!from.isEmpty()) {
+				to.push(from.top());
+				from.pop();
+			}
+		}
+	}
+	
+    /**
+     *  &Uuml;berpr&uuml;ft, ob die Warteschlange leer ist.
+     */
+    public boolean isEmpty() {
+    	if (upperfirstofqueue.isEmpty() && upperlastofqueue.isEmpty()) {
+    		return true;
+    	}
+    	else return false;
+    }
+
+    /**
+     *  Ein Element wird hinten an die Warteschlange angef&uuml;gt.
+     *  @param element Die anzuf&uuml;gende Objektreferenz.
+     */
+    public void enqueue(T element) {
+    	this.restack(upperfirstofqueue, upperlastofqueue);
+    	upperlastofqueue.push(element);
+    }
+
+    /**
+     *  Liefert das vorderste Element - so vorhanden - zur&uuml;ck.
+     *  @return Vorderstes Element (Objektreferenz)
+     *  @throws IllegalStateException falls die Warteschlange leer ist.
+     */
+    public T front() throws IllegalStateException {
+    	if (this.isEmpty()) {
+    		throw new IllegalStateException("Warteschlange ist leer.");
+    	}
+    	else {
+    		this.restack(upperlastofqueue, upperfirstofqueue);
+    		return upperfirstofqueue.top();
+    	}
+    }
+
+    /**
+     *  Das erste Element der Warteschlange - so vorhanden - wird entfernt.
+     *  @throws IllegalStateException falls die Warteschlange leer ist.
+     */
+    public void dequeue() throws IllegalStateException {
+    	if (this.isEmpty()) {
+    		throw new IllegalStateException("Warteschlange ist leer.");
+    	}
+    	else {
+    		this.restack(upperlastofqueue, upperfirstofqueue);
+    		upperfirstofqueue.pop();
+    	}
+    }
 } 
---
+++ A07_ADTQueue/src/WarteschlangeTest.java
@@ -0,0 +1,58 @@
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.*;
+
+import org.junit.Test;
+
+/** Beispiele zu Assertions in JUnit 5
+ * @see https://junit.org/junit5/docs/5.0.1/api/org/junit/jupiter/api/Assertions.html
+ */
+public class WarteschlangeTest {
+
+	@Test
+	public void test() {
+
+		// Erzeuge Warteschlange zum Testen.
+		Warteschlange<Integer> w = new Warteschlange<Integer>();
+		
+		assertEquals("Warteschlange wird nicht als leer erkannt.", true, w.isEmpty());
+		
+		w.enqueue(0);
+		
+		assertEquals("Warteschlange wird als leer erkannt.", false, w.isEmpty());
+		assertEquals("Das vorderste Element der Warteschlange wird nicht korekkt erkannt.", (Integer) 0, w.front());
+		
+		w.enqueue(2147483647);
+		w.enqueue(1000);
+		w.enqueue(1);
+		
+		assertEquals("Warteschlange wird als leer erkannt.", false, w.isEmpty());
+		assertEquals("Das vorderste Element der Warteschlange wird nicht korekkt erkannt.", (Integer) 0, w.front());
+		
+		w.dequeue();
+		
+		assertEquals("Warteschlange wird als leer erkannt.", false, w.isEmpty());
+		assertEquals("Das vorderste Element der Warteschlange wird nicht korekkt erkannt.", (Integer) 2147483647, w.front());
+		
+		w.dequeue();
+		w.dequeue();
+		w.dequeue();
+		
+		assertEquals("Warteschlange wird nicht als leer erkannt.", true, w.isEmpty());
+
+	}
+
+	@Test
+	public void testException() {
+		
+		// Erzeuge Warteschlange zum Testen.
+		Warteschlange<Integer> w = new Warteschlange<Integer>();
+		
+		assertThrows(IllegalStateException.class, () -> { w.front(); }, "Leere Warteschlange wirft keinen Fehler bei Anwendung der front()-Methode.");
+		
+		assertThrows(IllegalStateException.class, () -> { w.dequeue(); }, "Leere Warteschlange wirft keinen Fehler bei Anwendung der dequeue()-Methode.");
+		
+	}
+
+}
