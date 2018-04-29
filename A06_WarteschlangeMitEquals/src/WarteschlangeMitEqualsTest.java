efbd1a3e577b907ff204ecab8897b27311d01bdc
JFJ0831 <cq95678@googlemail.com>
2018-04-29 23:24:46 +0200

    v1.0 completed tests

--- A06_WarteschlangeMitEquals/src/WarteschlangeMitEqualsTest.java
+++ A06_WarteschlangeMitEquals/src/WarteschlangeMitEqualsTest.java
@@ -1,7 +1,6 @@
 import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.CoreMatchers.*;
 
 import org.junit.Test;
 
@@ -11,45 +10,110 @@ import org.junit.Test;
 public class WarteschlangeMitEqualsTest {
 
 	@Test
-	public void testNotNull() {
+	public void testStandartMethods() {
 
-		// Erzeuge Objekt zum Testen.
-		String s = "Ein Test";
+		// Erzeuge Warteschlange zum Testen.
+		WarteschlangeMitEquals<Integer> w = new WarteschlangeMitEquals<Integer>();
 		
-		// Dieser Test ist inhaltlich wenig zielfuehrend,
-		// sondern zeigt nur die Syntax von 'assertThat'
-		assertThat("Strings werden nicht korrekt verglichen.",
-				s,
-				is(not(equalTo("Ein anderer Test"))));
-				
+		
+		assertEquals("Warteschlange wird nicht als leer erkannt.", true, w.isEmpty());
+		
+		w.enqueue(0);
+		
+		assertEquals("Warteschlange wird als leer erkannt.", false, w.isEmpty());
+		assertEquals("Das vorderste Element der Warteschlange wird nicht korekkt erkannt.", (Integer) 0, w.front());
+		
+		w.enqueue(2147483647);
+		w.enqueue(1000);
+		w.enqueue(1);
+		
+		assertEquals("Warteschlange wird als leer erkannt.", false, w.isEmpty());
+		assertEquals("Das vorderste Element der Warteschlange wird nicht korekkt erkannt.", (Integer) 0, w.front());
+		
+		w.dequeue();
+		
+		assertEquals("Warteschlange wird als leer erkannt.", false, w.isEmpty());
+		assertEquals("Das vorderste Element der Warteschlange wird nicht korekkt erkannt.", (Integer) 2147483647, w.front());
+		
+		w.dequeue();
+		w.dequeue();
+		w.dequeue();
+		
+		assertEquals("Warteschlange wird nicht als leer erkannt.", true, w.isEmpty());
+
 	}
+	
+	@Test
+	public void testEquals() {
+
+		// Erzeuge Warteschlangen zum Testen.
+		WarteschlangeMitEquals<Integer> wme1 = new WarteschlangeMitEquals<Integer>();
+		WarteschlangeMitEquals<Integer> wme2 = new WarteschlangeMitEquals<Integer>();
+		WarteschlangeMitEquals<Integer> wme3 = new WarteschlangeMitEquals<Integer>();
+		WarteschlangeMitEquals<Integer> wme4 = new WarteschlangeMitEquals<Integer>();
+		WarteschlangeMitEquals<Integer> wme5 = new WarteschlangeMitEquals<Integer>();
+		WarteschlangeMitEquals<Integer> wme6 = new WarteschlangeMitEquals<Integer>();
+		
+		// Modifiziere Warteschlangen zum Testen.
+		// WME1
+		wme1.enqueue(0);
+		wme1.enqueue(99);
+		wme1.enqueue(600);
+		wme1.enqueue(30);
+		wme1.enqueue(4567);
+		wme1.enqueue(97421);
+		// WME2
+		wme2.enqueue(0);
+		wme2.enqueue(99);
+		wme2.enqueue(600);
+		wme2.enqueue(30);
+		wme2.enqueue(4567);
+		wme2.enqueue(97421);
+		// WME3
+		wme3.enqueue(0);
+		wme3.enqueue(99);
+		wme3.enqueue(600);
+		wme3.enqueue(30);
+		wme3.enqueue(4567);
+		// WME4
+		wme4.enqueue(0);
+		wme4.enqueue(99);
+		wme4.enqueue(4567);
+		wme4.enqueue(568932);
+		wme4.enqueue(345);
+		wme4.enqueue(9876);
+		// WME5
+		wme5.enqueue(3);
+		wme5.dequeue();
+		
+		// Testen
+		assertEquals("Warteschlangen werden nicht als identisch erkannt", true, wme1.equals(wme2));
+		
+		assertEquals("Warteschlangen werden als identisch erkannt", false, wme1.equals(wme3));
+		
+		assertEquals("Warteschlangen werden als identisch erkannt", false, wme4.equals(wme3));
+		
+		assertEquals("Leere Warteschlangen werden nicht als identisch erkannt", true, wme5.equals(wme6));
+		
+		assertEquals("Warteschlange wird als identisch mit null erkannt", false, wme1.equals(null));
+		
+		assertEquals("Warteschlange wird als identisch mit leerer Warteschlange erkannt", false, wme1.equals(wme6));
+		
+	}
+
 
 	@Test
 	public void testException() {
-				
-		// Bereite Feld zum Testen vor.
-		int[] i = { 1, 2 };
 		
-		// Informell beschriebene Syntax von assertThrows:
-		// 1. Parameter: Name der erwarteten Ausnahme erweitert um ".class". 
-		//               In diesem Fall: ArrayIndexOutOfBoundsException.class
-		// 2. Parameter: Lambda-Ausdruck [ja, so etwas gibt es in Java, https://docs.oracle.com/javase/8/docs/api/java/lang/FunctionalInterface.html]
-		//               der Form "Parameterliste -> Funktionsrumpf". In diesem
-		//               Fall: () [= keine Parameter] -> { i[3] = 5; }
-		// 3. Parameter: Text der Fehlermeldung, falls Ausnahme erzeugt wird.
-		//
-		// Achtung: Die im Funktionsrumpf getesteten Variablen duerfen nach
-		//          dem Rumpf (also z.B. in Vorbereitung eines weiteren Tests)
-		//          nicht mehr veraendert werden, sind also aus Sicht des Tests
-		//          konstant. Andernfalls wird ein Fehler bei der Kompilation erzeugt.
-		// Allgemeine Dokumentation:
-		// https://junit.org/junit5/docs/5.0.1/api/org/junit/jupiter/api/Assertions.html#assertThrows-java.lang.Class-org.junit.jupiter.api.function.Executable-java.lang.String-
-		assertThrows(ArrayIndexOutOfBoundsException.class, 
-				() -> { 
-					i[3] = 5;
-					}, 
-				"Zugriff ausserhalb der Feldgrenzen wird nicht gemeldet.");
+		// Erzeuge Warteschlange zum Testen.
+		WarteschlangeMitEquals<Integer> w = new WarteschlangeMitEquals<Integer>();
+		
+		assertThrows(IllegalStateException.class, () -> { w.front(); }, "Leere Warteschlange wirft keinen Fehler bei Anwendung der front()-Methode.");
+		
+		assertThrows(IllegalStateException.class, () -> { w.dequeue(); }, "Leere Warteschlange wirft keinen Fehler bei Anwendung der dequeue()-Methode.");
 		
 	}
+				
+	
 
 }
