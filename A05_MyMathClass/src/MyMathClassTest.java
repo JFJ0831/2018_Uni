d2b50f52d31d24828f056265c85f5bc958ea460a
JFJ0831 <cq95678@googlemail.com>
2018-04-30 05:39:07 +0200

    v1.0 added tests and comments

--- A05_MyMathClass/src/MyMathClassTest.java
+++ A05_MyMathClass/src/MyMathClassTest.java
@@ -1,55 +1,87 @@
 import static org.junit.Assert.*;
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Vector;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.CoreMatchers.*;
 
 import org.junit.Test;
 
-/** Beispiele zu Assertions in JUnit 5
- * @see https://junit.org/junit5/docs/5.0.1/api/org/junit/jupiter/api/Assertions.html
- */
 public class MyMathClassTest {
-
+	
 	@Test
-	public void testNotNull() {
-
-		// Erzeuge Objekt zum Testen.
-		String s = "Ein Test";
+	public void testStandardDeviation() {
 		
-		// Dieser Test ist inhaltlich wenig zielfuehrend,
-		// sondern zeigt nur die Syntax von 'assertThat'
-		assertThat("Strings werden nicht korrekt verglichen.",
-				s,
-				is(not(equalTo("Ein anderer Test"))));
-				
+		MyMathClass mmc = new MyMathClass();
+		Vector<Number> vec = new Vector<Number>();
+		
+		vec.addElement(9);		
+		vec.addElement(-45);
+		vec.addElement(10.6);
+		vec.addElement(null);
+		vec.addElement(0);
+		
+		assertThat("Standartabweichung wird nicht korrekt berechnet.", mmc.standardDeviation(vec), is(equalTo(22.677466789745277896181082151853)));
 	}
 
+	/**
+	 * Damit die folgenden Tests funktionieren müssen die zu Testenden Hilfsmethoden erst Paketsichtbarkeit erhalten.
+	 */
+	/*
 	@Test
-	public void testException() {
-				
-		// Bereite Feld zum Testen vor.
-		int[] i = { 1, 2 };
+	public void testCalculateMean() {
 		
-		// Informell beschriebene Syntax von assertThrows:
-		// 1. Parameter: Name der erwarteten Ausnahme erweitert um ".class". 
-		//               In diesem Fall: ArrayIndexOutOfBoundsException.class
-		// 2. Parameter: Lambda-Ausdruck [ja, so etwas gibt es in Java, https://docs.oracle.com/javase/8/docs/api/java/lang/FunctionalInterface.html]
-		//               der Form "Parameterliste -> Funktionsrumpf". In diesem
-		//               Fall: () [= keine Parameter] -> { i[3] = 5; }
-		// 3. Parameter: Text der Fehlermeldung, falls Ausnahme erzeugt wird.
-		//
-		// Achtung: Die im Funktionsrumpf getesteten Variablen duerfen nach
-		//          dem Rumpf (also z.B. in Vorbereitung eines weiteren Tests)
-		//          nicht mehr veraendert werden, sind also aus Sicht des Tests
-		//          konstant. Andernfalls wird ein Fehler bei der Kompilation erzeugt.
-		// Allgemeine Dokumentation:
-		// https://junit.org/junit5/docs/5.0.1/api/org/junit/jupiter/api/Assertions.html#assertThrows-java.lang.Class-org.junit.jupiter.api.function.Executable-java.lang.String-
-		assertThrows(ArrayIndexOutOfBoundsException.class, 
-				() -> { 
-					i[3] = 5;
-					}, 
-				"Zugriff ausserhalb der Feldgrenzen wird nicht gemeldet.");
+		MyMathClass mmc = new MyMathClass();
+		Vector<Number> vec = new Vector<Number>();
 		
+		vec.addElement(9);
+		
+		assertEquals("Arithmetisches Mittel wird nicht korekkt berechnet.", 9, mmc.calculateMean(vec), 0);
+		
+		vec.addElement(-45);
+		vec.addElement(10.6);
+		vec.addElement(0);
+		// null-referenz kann nicht im Vektor enthalten sein, da es sich eigentlich um eine private Methode handelt.
+		assertEquals("Arithmetisches Mittel wird nicht korekkt berechnet.", -6.35, mmc.calculateMean(vec), 0);
 	}
-
+	
+	@Test
+	public void testCalculateVariance() {
+		
+		MyMathClass mmc = new MyMathClass();
+		Vector<Number> vec = new Vector<Number>();
+		
+		vec.addElement(9);
+		
+		assertEquals("Varianz wird nicht korekkt berechnet.", 0, mmc.calculateVariance(vec), 0);
+		
+		vec.addElement(-45);
+		vec.addElement(10.6);
+		vec.addElement(0);
+		
+		assertEquals("Varianz wird nicht korekkt berechnet.", 514.2675, mmc.calculateVariance(vec), 0);
+	}
+	
+	@Test
+	public void testRemoveNullElements() {
+		
+		MyMathClass mmc = new MyMathClass();
+		Vector<Number> vec1 = new Vector<Number>();
+		Vector<Number> vec2 = new Vector<Number>();
+		
+		vec1.addElement(9);		
+		vec1.addElement(-45);
+		vec1.addElement(10.6);
+		vec1.addElement(null);
+		vec1.addElement(0);
+		
+		vec2.addElement(9);		
+		vec2.addElement(-45);
+		vec2.addElement(10.6);
+		vec2.addElement(0);
+		
+		assertThat("null-Referenzen werden nicht korrekt entfernt.", mmc.removeNullElements(vec1), is(equalTo(vec2)));
+	}
+	*/
 }
