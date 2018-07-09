fe7b6016c4f86bbc8ad459e65e0624b4d6c536c4
JFJ0831 <cq95678@googlemail.com>
2018-07-09 08:11:47 +0200

    v0.9

--- A42_Graphenalgorithmen/src/GraphAlgorithmen.java
+++ A42_Graphenalgorithmen/src/GraphAlgorithmen.java
@@ -1,5 +1,8 @@
 import java.util.Vector;
 
+import Jama.EigenvalueDecomposition;
+import Jama.Matrix;
+
 /**
  *  Sammlung verschiedener Graphalgorithmen.
  */
@@ -17,9 +20,13 @@ public class GraphAlgorithmen {
 		// Erzeuge einen Vektor zum Speichern der berechneten Zentralitaeten
 		Vector<Double> zentralitaeten = new Vector<Double>(graph.getAnzahlKnoten());
 		
-		/*
-		 * Programmtext hier ergaenzen.
-		 */
+		// Schleife über die Anzahl der Knoten im Graphen.
+		for (int i = 0; i < graph.getAnzahlKnoten(); i++) {
+			// Normalisierung des Knotengrades bzgl. des maximal möglichen Knotengrads.
+			double normMaxMoeglich = graph.getKnotengrad(i) / (graph.getAnzahlKnoten() - 1);
+			// Hinzufuegen des normalisierten Knotengrads an der Stelle i zu dem Vektor.
+			zentralitaeten.add(i, normMaxMoeglich);
+		}
 		
 		// Gib den Vektor der berechneten Werte zurueck
 		return zentralitaeten;
@@ -37,9 +44,24 @@ public class GraphAlgorithmen {
 		// Erzeuge einen Vektor zum Speichern der berechneten Zentralitaeten
 		Vector<Double> zentralitaeten = new Vector<Double>(graph.getAnzahlKnoten());
 		
-		/*
-		 * Programmtext hier ergaenzen.
-		 */
+		// Initialisierung des maximal auftretenden Knotengrads mit dem Wert 0.
+		int maxAuftretend = 0;
+		
+		// Schleife über die Anzahl der Knoten im Graphen.
+		for (int i = 0; i < graph.getAnzahlKnoten(); i++) {
+			// Speichern des aktuellen Knotengrads, falls dieser groesser als alle vorherigen ist.
+			if (graph.getKnotengrad(i) > maxAuftretend) {
+				maxAuftretend = graph.getKnotengrad(i);
+			}
+		}
+		
+		// Schleife über die Anzahl der Knoten im Graphen.
+		for (int i = 0; i < graph.getAnzahlKnoten(); i++) {
+			// Normalisierung des Knotengrades bzgl. des maximal auftretenden Knotengrads.
+			double normMaxAuftretend = graph.getKnotengrad(i) / maxAuftretend;
+			// Hinzufuegen des normalisierten Knotengrads an der Stelle i zu dem Vektor.
+			zentralitaeten.add(i, normMaxAuftretend);
+		}
 		
 		// Gib den Vektor der berechneten Werte zurueck
 		return zentralitaeten;	
@@ -57,9 +79,22 @@ public class GraphAlgorithmen {
 		// Erzeuge einen Vektor zum Speichern der berechneten Zentralitaeten
 		Vector<Double> zentralitaeten = new Vector<Double>(graph.getAnzahlKnoten());
 
-		/*
-		 * Programmtext hier ergaenzen.
-		 */
+		// Initialisierung der Summe der Knotengrade mit 0.
+		int summe = 0;
+		
+		// Schleife über die Anzahl der Knoten im Graphen.
+		for (int i = 0; i < graph.getAnzahlKnoten(); i++) {
+			// Hinzufuegern des aktuellen Knotengrads zur Summe der Knotengrade.
+			summe += graph.getKnotengrad(i);
+		}
+		
+		// Schleife über die Anzahl der Knoten im Graphen.
+		for (int i = 0; i < graph.getAnzahlKnoten(); i++) {
+			// Normalisierung des Knotengrades bzgl. der Summe der Knotengrade.
+			double normSumme = graph.getKnotengrad(i) / summe;
+			// Hinzufuegen des normalisierten Knotengerads an der Stelle i zu dem Vektor.
+			zentralitaeten.add(i, normSumme);
+		}
 
 		// Gib den Vektor der berechneten Werte zurueck
 		return zentralitaeten;	
@@ -77,20 +112,28 @@ public class GraphAlgorithmen {
 		Vector<Double> eigenvektor = new Vector<Double>(graph.getAnzahlKnoten());
 
 		// Berechne die Eigenwertzerlegung der transponierten Adjazenzmatrix
-
-		
+		Matrix transponierteAdjazenzMatrix = graph.getAdjazenzMatrix().transpose();
+		EigenvalueDecomposition eigenwertZerlegung = transponierteAdjazenzMatrix.eig();
 		
 		// Finde den groessten Eigenwert, indem die reellen Teile der
 		// zuvor berechneten Zerlegung betrachtet werden.
-
-		
+		double[] reelleEigenwerte = eigenwertZerlegung.getRealEigenvalues();
+		int indexGroessterEigenwert = 0;
+		for (int i = 1; i < reelleEigenwerte.length; i++) {
+			if (reelleEigenwerte[i] > reelleEigenwerte[indexGroessterEigenwert]) {
+				indexGroessterEigenwert = i;
+			}
+		}
+		double groessterEigenwert = reelleEigenwerte[indexGroessterEigenwert];
 		
 		// Lies die Eigenvektoren zu den Eigenwerten aus.
-
+		Matrix eigenvektoren = eigenwertZerlegung.getV();
 		
 		
 		// Kopiere den Eigenvektor zum groessten Eigenwert.
-
+		for (int i = 0; i < graph.getAnzahlKnoten(); i++) {
+		eigenvektor.add(i, eigenvektoren.get(i, indexGroessterEigenwert));
+		}
 
 		// Gebe den berechneten Eigenvektor zurueck.
 		return eigenvektor;
